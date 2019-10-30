fb1ea25189c2e756ed1e5bb44aa47b7c752368d5
wangjiacheng <selfimpr@yeah.net>
2019-10-30 14:11:22 +0800

    增加网络加载svga事例

--- app/src/main/java/com/selfimpr/svgaplayerdemo/MainActivity.java
+++ app/src/main/java/com/selfimpr/svgaplayerdemo/MainActivity.java
@@ -13,27 +13,51 @@ import com.opensource.svgaplayer.SVGAImageView;
 import com.opensource.svgaplayer.SVGAParser;
 import com.opensource.svgaplayer.SVGAVideoEntity;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static List<String> SVGAUrls;
+
+    static {
+        SVGAUrls = new ArrayList<>();
+        SVGAUrls.add("https://yjmf.bs2dl.yy.com/Yjk3YTdlNDEtNzFhZC00ZTljLWE1NTAtMTA1OGZhYmM5ZTU2.svga");
+        SVGAUrls.add("https://yjmf.bs2dl.yy.com/MjQ0ZWQzODYtMTJjNC00ZDU4LTlkOTgtZmVkMWI2NzRjMjNh.svga");
+        SVGAUrls.add("https://yjmf.bs2dl.yy.com/YjQ0YzQxZmItNmZjNy00ZGFmLWIyOWQtODdhMjdkN2VmMzhj.svga");
+        SVGAUrls.add("https://yjmf.bs2dl.yy.com/YTljY2E2ZjUtN2U4YS00NzA4LWJjNDEtMDE0ZWU4OWI4ZWM5.svga");
+        SVGAUrls.add("https://yjmf.bs2dl.yy.com/MzBiOWM0ZDQtNDI3OS00MGYyLWE2ZDktYmZiYjczOTA1NGFi.svga");
+        SVGAUrls.add("https://yjmf.bs2dl.yy.com/MDA0N2ZiNDctNTY3MS00M2ZlLWEzZGYtYzMzNjQ1ZWFmZGRj.svga");
+        SVGAUrls.add("https://yjmf.bs2dl.yy.com/MzZjMTIzYmUtODQ5NC00NmJjLTg1MjktMmM2MjZmMWJiYTAw.svga");
+        SVGAUrls.add("https://yjmf.bs2dl.yy.com/YzY2N2M5MTYtZDdlZS00ZTQyLWE2MGYtNjE1YzZhMWIwYzFk.svga");
+        SVGAUrls.add("https://yjmf.bs2dl.yy.com/Njg2YmYzN2EtODM1Ny00OGE1LTkwN2ItOTYwOGY3NDRiMTAw.svga");
+        SVGAUrls.add("https://yjmf.bs2dl.yy.com/YmE1N2ViMjAtMTFmOC00ZTkwLTliYzktNjk0NGExMWU4ZWZh.svga");
+    }
+
+    private int mClickCount;
+
     private SVGAImageView mSVGAImageView;
+    private SVGAParser mSVGAParser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSVGAImageView = findViewById(R.id.svga);
+        mSVGAParser = new SVGAParser(this);
 
-        String url = "https://yjmf.bs2dl.yy.com/Yjk3YTdlNDEtNzFhZC00ZTljLWE1NTAtMTA1OGZhYmM5ZTU2.svga";
-        url = "https://yjmf.bs2dl.yy.com/MjQ0ZWQzODYtMTJjNC00ZDU4LTlkOTgtZmVkMWI2NzRjMjNh.svga";
+//        loadAnimationFromAssets();
 
-        loadAnimation();
+        loadAnimationFromNet(SVGAUrls.get(0));
     }
 
     /**
      * http://svga.io/intro.html
      */
-    private void loadAnimation() {
-        mSVGAImageView = findViewById(R.id.svga);
-        SVGAParser svgaParser = new SVGAParser(this);
-        svgaParser.decodeFromAssets("bid.svga", new SVGAParser.ParseCompletion() {
+    private void loadAnimationFromAssets() {
+        mSVGAParser.decodeFromAssets("bid.svga", new SVGAParser.ParseCompletion() {
             @Override
             public void onComplete(SVGAVideoEntity mSVGAVideoEntity) {
                 mSVGAImageView.setVisibility(View.VISIBLE);
@@ -62,11 +86,13 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "解析失败", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
-        /*try { // new URL needs try catch.
-            svgaParser.parse(new URL("http://github.com/yyued/SVGAPlayer-Android/blob/master/app/src/main/assets/heartbeat.svga"), new SVGAParser.ParseCompletion() {
+    private void loadAnimationFromNet(String url) {
+        try {
+            mSVGAParser.decodeFromURL(new URL(url), new SVGAParser.ParseCompletion() {
                 @Override
-                public void onComplete(@NotNull SVGAVideoEntity videoItem) {
+                public void onComplete(SVGAVideoEntity videoItem) {
                     mSVGAImageView.setVideoItem(videoItem);
                     mSVGAImageView.startAnimation();
                 }
@@ -79,6 +105,11 @@ public class MainActivity extends AppCompatActivity {
         } catch (MalformedURLException e) {
             e.printStackTrace();
             Toast.makeText(MainActivity.this, "解析失败：" + e, Toast.LENGTH_SHORT).show();
-        }*/
+        }
+    }
+
+    public void next(View view) {
+        mClickCount++;
+        loadAnimationFromNet(SVGAUrls.get(mClickCount % (SVGAUrls.size() - 1)));
     }
 }
